b748becdfd7f20742586c4291c093a00a9891c8b
Bogdan <tischenkob@gmail.com>
2019-12-02 20:38:29 +0300

    first-time access fix

--- src/service/api.js
+++ src/service/api.js
@@ -1,14 +1,19 @@
 import axios from "axios";
 
 export default () => {
-  let currentUser = JSON.parse(window.localStorage.currentUser);
+  let currentUser;
+  try {
+    currentUser = JSON.parse(window.localStorage.currentUser);
+  } catch (err) {
+    currentUser = null;
+  }
   return axios.create({
     baseURL: "http://localhost:8082/api",
     withCredentials: false,
     headers: {
       Accept: "application/json",
       "Content-Type": "application/json",
-      Authorisation: currentUser && currentUser.token
+      Authorisation: currentUser.token || ""
     }
   });
 };
